490a0a4c7debf185f631d7e9f0b9247fa9cd3426
Luca Tescari <luca@ltescari.ch>
2026-03-30 09:24:52 +0200

    Update gitveil to 0.3.1

--- Formula/gitveil.rb
+++ Formula/gitveil.rb
@@ -1,17 +1,13 @@
 class Gitveil < Formula
   desc "Transparent file encryption in git (git-crypt compatible)"
   homepage "https://github.com/lucatescari/gitveil"
-  version "0.3.0"
+  version "0.3.1"
   license "GPL-3.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/lucatescari/gitveil/releases/download/v0.2.1/gitveil-0.2.1-darwin-arm64.tar.gz"
-      sha256 "ac9e14e1092ff61d12d5bc18623394a923641aeab2ff1bfb403006d6842bfa89"
-    end
-    on_intel do
-      url "https://github.com/lucatescari/gitveil/releases/download/v0.2.1/gitveil-0.2.1-darwin-arm64.tar.gz"
-      sha256 "ac9e14e1092ff61d12d5bc18623394a923641aeab2ff1bfb403006d6842bfa89"
+      url "https://github.com/lucatescari/gitveil/releases/download/v0.3.1/gitveil-0.3.1-darwin-arm64.tar.gz"
+      sha256 "de46e0a33d4d56f0481bf1e5fb0943bfc7156c72ab99eb325b20714a3a4d9837"
     end
   end
 
@@ -20,6 +16,6 @@ class Gitveil < Formula
   end
 
   test do
-    assert_match "gitveil", shell_output("#{bin}/gitveil --version")
+    assert_match "gitveil", shell_output("\#{bin}/gitveil --version")
   end
 end
