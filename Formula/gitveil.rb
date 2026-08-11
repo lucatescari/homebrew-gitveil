457baebb9e8a50b0a562f592b9b50f2770b6a23d
Luca Tescari <luca@ltescari.ch>
2026-08-11 13:40:45 +0200

    Update gitveil to 1.3.2

--- Formula/gitveil.rb
+++ Formula/gitveil.rb
@@ -1,13 +1,13 @@
 class Gitveil < Formula
   desc "Transparent file encryption in git (git-crypt compatible)"
   homepage "https://github.com/lucatescari/gitveil"
-  version "1.3.1"
+  version "1.3.2"
   license "GPL-3.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/lucatescari/gitveil/releases/download/v1.3.1/gitveil-1.3.1-darwin-arm64.tar.gz"
-      sha256 "f5e314ffba23eeaa9b623aad705d4e0ad8049d52f41692d7fca9cea816e58fc2"
+      url "https://github.com/lucatescari/gitveil/releases/download/v1.3.2/gitveil-1.3.2-darwin-arm64.tar.gz"
+      sha256 "a83dfb6f31e0221926beab166752f5d78aaa0641bcf0384433389780f19322a9"
     end
   end
 
